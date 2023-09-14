6fcf0f3d737ae1e1d8dd5e62c2c1d54ed1888e8a
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-09-14 06:56:18 -0700

    improvement: add colored errors, adjust arg name

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 // dependencies
 use clap::Parser;
+use color_eyre::eyre::Result;
 use std::fs::File;
 use std::io::{self, Write};
 
@@ -10,15 +11,27 @@ use std::io::{self, Write};
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    name: String,
+    file: String,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
+    // initialize color_eyre
+    color_eyre::install()?;
+
+    // get the command line arguments
     let args = Args::parse();
+
+    // create a handle for writing output
     let mut stdout = io::stdout();
-    let new_file = File::create(args.name);
+
+    // create the new file
+    let new_file = File::create(args.file);
+
+    // write a success message or an error if the file couldn't be created
     match new_file {
-        Ok(file) => write!(stdout, "{:#?} successfully created", file),
-        Err(err) => write!(stdout, "{:#?}", err),
+        Ok(file) => write!(stdout, "{:#?} successfully created", file)?,
+        Err(err) => write!(stdout, "{:#?}", err)?,
     }
+
+    Ok(())
 }
